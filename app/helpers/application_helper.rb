88fb3e3537d6d9a82c2d22609a3749a41ad23ade
pluto2357 <futureedokkojony5@gmail.com>
2016-03-16 15:34:10 +0900

    gemfileの更新/投稿機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
-module ApplicationHelper
-end
+    module ApplicationHelper
+    def br(str)
+      h(str).gsub(/(\r\n?)|(\n)/, "<br />").html_safe
+    end
+  end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get'/tweets'=>'tweets#index'
+  root 'tweets#index'
+  get'/tweets/new'=>'tweets#new'
+  post'/tweets'=>'tweets#create'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
